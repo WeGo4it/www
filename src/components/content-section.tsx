a3adb09abb12d308cd41fe4c1d5a239625fc8cf1
geertclaes-rwe <UI517987@rwe.com>
2025-03-20 12:54:04 +0000

    Initial commit

---
+++ src/components/content-section.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import { ChevronRight } from 'lucide-react'
+import Link from 'next/link'
+
+export default function ContentSection() {
+    return (
+        <section className="py-16 md:py-32">
+            <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-12">
+                <img className="rounded-(--radius) grayscale" src="/images/programmer-working-on-a-computer-in-the-office-at-night.jpg" alt="team image" height="" width="" loading="lazy" />
+
+            </div>
+        </section>
+    )
+}
